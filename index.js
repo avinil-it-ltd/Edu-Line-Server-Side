e4d4530712cfe2288670d43d8ce7da6ddae0eb41
Avishekdevnath <avishekdevnath@gmail.com>
2023-06-14 01:12:26 +0600

    A

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-//sadiabinte63
-//Godo0Y78VrUIRLes
+
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +26,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // const languageCollection = client.db('languageDb').collection('instructors');
+    // const languageCollection = client.db('languageDb').collection('Classes');
+
+
+    const classes = client.db('languageDb').collection("classes");
+    const instructors = client.db('languageDb').collection("instructors");
+    const reviewers = client.db('languageDb').collection("reviewer");
+    const SelectedClasses = client.db('languageDb').collection("SelectClasses");
+    const usersCollection = client.db('languageDb').collection("users");
+  
+   //  Select Class Collection
+   app.post('/api/classes', async (req, res) => {
+    const item = req.body;
+    console.log(item)
+    const result = await classes.insertOne(item);
+    res.send(result);
+  })
+
+    app.get('/api/classes', async (req, res) => {
+      const result = await classes.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/api/instructors', async (req, res) => {
+      const result = await instructors.find().toArray();
+      res.send(result);
+    })
+
+     
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
